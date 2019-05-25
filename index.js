69e7263b25fc85aafbf959e2109a69e61b870157
Reyaad Rafeek <reyaad510@gmail.com>
2019-05-25 13:21:50 -0400

    imported knex, created knexConfig, .GET functionality completed

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+const knexConfig = {
+  client: 'sqlite3',
+  connection: {
+    filename: './data/lambda.sqlite3'
+  },
+  useNullAsDefault: true
+}
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 
+const db = knex(knexConfig);
+
+
 // endpoints here
+
+server.get('/api/zoos', (req, res) => {
+  db('zoos')
+  .then(zoos => {
+    res.status(200).json(zoos);
+  }).catch(err => {
+    console.log(err);
+  })
+})
+
 
 const port = 3300;
 server.listen(port, function() {
